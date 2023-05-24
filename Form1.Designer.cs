f3c0a1ad668babc7cc480bf148b638392f3e3f29
Doia-e9 <leonardogh75@gmail.com>
2023-05-24 13:01:15 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ProyectoFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonEliminar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonEliminar
+            // 
+            this.ButtonEliminar.Location = new System.Drawing.Point(61, 216);
+            this.ButtonEliminar.Name = "ButtonEliminar";
+            this.ButtonEliminar.Size = new System.Drawing.Size(75, 23);
+            this.ButtonEliminar.TabIndex = 0;
+            this.ButtonEliminar.Text = "Eliminar";
+            this.ButtonEliminar.UseVisualStyleBackColor = true;
+            this.ButtonEliminar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(61, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(548, 135);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(8, 10);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Location = new System.Drawing.Point(515, 124);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(100, 20);
+            this.txtCategoria.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCategoria);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ButtonEliminar);
+            this.Name = "Form1";
+            this.Text = "Gestor de Gastos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonEliminar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClaseObjeto;
+
+namespace ProyectoFinal
+{
+    public partial class Form1 : Form
+    {
+        List<Gasto> ListaGastos = new List<Gasto>();
+        int presupuesto=0;
+        public Form1()
+        {
+           
+           
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void botonAgregar_Click(object sender, EventArgs e)
+        {
+            //Espacio para agregar el texto 
+            Gasto NuevoGasto = new Gasto();
+            //textbox Agregar Categoria
+            //Llamar el array para seleccionar la categoria
+            NuevoGasto.Categoria = txtCategoria.Text.TrimEnd();
+
+            //textbox Agregar Fecha
+            NuevoGasto.Fecha = txtFecha.Text.TrimEnd();
+
+            //textbox Agregar Descripcion
+            NuevoGasto.Descripcion = txtDescripcion.Text.TrimEnd();
+
+            //textbox Agregar Monto
+            NuevoGasto.Monto = txtMonto.Text.TrimEnd();
+
+            //Este dato siempre se incrementará sin tener que ecceder a dicho dato
+            //El indice comienza desde cero
+            NuevoGasto.ID = NuevoGasto.ID;
+
+           presupuesto= NuevoGasto.Monto
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int UserId = txtMonto.Text.TrimEnd(); 
+
+           //Boton eliminar
+            //A continuación se va a desplegar todos los datos que se encuentran almacenados, para que de esta forma,se generen los datos que se desean cambiar
+            //Se va a utilizar una lisview para mostrar los datos
+            //Iterador será la variable del mismo tipo que el opbjeto, el cual nos va a servir para igualar los datos 
+            foreach(Gasto iterador  in ListaGastos)
+            {
+                //Se resta un dato porque el conteo de objetos comienza en 0
+                if(iterador.ID == UserId-1)
+                {
+                    //Al  final se remueve el dato que tenga relacion con el dato buscado
+                    ListaGastos.RemoveAt(iterador.ID);
+                }
+              
+
+
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Agregar presupuesto 
+        }
+    }
+}
